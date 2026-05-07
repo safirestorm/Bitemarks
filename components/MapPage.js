0aa305fa995b94790cb60098dba75658c42f1aee
safirestorm <safa0003@stud.ek.dk>
2026-05-07 12:31:26 +0200

    Location implemented on map

--- components/MapPage.js
+++ components/MapPage.js
@@ -1,8 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
-import { useState } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { useEffect, useRef, useState } from "react";
+import * as Location from 'expo-location'
 
 export function MapPage() {
+  const [markers, setMarkers] = useState([])
+
   // Bestemmer hvilken andel af verdenen kortet viser
   const [region, setRegion] = useState({
     latitude:55.5,
@@ -11,13 +14,71 @@ export function MapPage() {
     longitudeDelta:6 
   })
 
+  // useRef kan holde en reference på tværs af renderings dvs at den ikke forsager en ny rendering når den ændres (som useState)
+  const mapView = useRef(null) // ref. til map view objektet
+  const locationSubscription = useRef(null) // når vi lukker appen, skal den ikke lytte mere
+
+  // useEffect kører hvor mange gange vi bestemmer og lukker ting vi har started ned når vi lukker appen
+  useEffect(() =>{
+    async function startListening() {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if(status !== 'granted'){
+        alert("ingen adgang til lokation")
+        return
+      }
+      locationSubscription.current = await Location.watchPositionAsync({
+        distanceInterval: 100,
+        accuracy: Location.Accuracy.Highest
+      }, (location) =>{
+        const newRegion = {
+          latitude:location.coords.latitude,
+          longitude:location.coords.longitude,
+          latitudeDelta:3, 
+          longitudeDelta:3
+        }
+        setRegion(newRegion) // flytter kortet til den nye location
+        if(mapView.current){
+          mapView.current.animateToRegion(newRegion)
+        }
+      })
+    }
+    startListening()
+    return () =>{
+      if(locationSubscription.current){
+        locationSubscription.current.remove()
+      }
+    }
+  }, []) // Et tomt array i slutningen betyder at denne useEffect kun skal køre én gang
+
+  function addPlace(data) {
+    const {latitude, longitude} = data.nativeEvent.coordinate
+    const newMarker = {
+      coordinate: {latitude, longitude},
+      key: data.timeStamp,
+      title:"Great Place"
+    }
+    setMarkers([...markers, newMarker])
+  }
+
+  function onPlacePressed(text) {
+     // Skal tilføjes en metode til hvad der skal vises når man trykker på en pin 
+  }
+
   return(
     <View>
       <MapView 
         style={styles.map}
         region={region}
+        onLongPress={addPlace}
       >
-          
+        {markers.map(marker =>(
+          <Marker
+            coordinate={marker.coordinate}
+            key={marker.key}
+            title={marker.title}
+            onPress={() => onPlacePressed(marker.title)}
+          />
+        ))}   
       </MapView> 
     </View>
   );
