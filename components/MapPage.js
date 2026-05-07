b276c2ad5366fe5e33918651b21c68ee9d6e2dda
safirestorm <safa0003@stud.ek.dk>
2026-05-07 11:04:01 +0200

    Added map to MapPage.js

--- components/MapPage.js
+++ components/MapPage.js
@@ -1,9 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+import { useState } from "react";
 
 export function MapPage() {
-    return(
-        <View>
-        <Text>Placeholder</Text>
-      </View>
-    );
+  // Bestemmer hvilken andel af verdenen kortet viser
+  const [region, setRegion] = useState({
+    latitude:55.5,
+    longitude:10.5,
+    latitudeDelta:6,  // Delta = Hvor mange grader vi skal vise/Hvor meget Zoom
+    longitudeDelta:6 
+  })
+
+  return(
+    <View>
+      <MapView 
+        style={styles.map}
+        region={region}
+      >
+          
+      </MapView> 
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    width:'100%',
+    height:'100%'
+  }
+})
